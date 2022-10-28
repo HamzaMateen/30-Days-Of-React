04fcd3fa73326a1c89a0f3101e4343ade94997c8
HamzaMateen <hamzamateen1583@gmail.com>
2022-10-28 21:11:56 +0500

    Day-06 Number Boxes solution added

--- 06_Day_Map_List_Keys/06_map_list_keys_boilerplate/src/index.js
+++ 06_Day_Map_List_Keys/06_map_list_keys_boilerplate/src/index.js
@@ -1,47 +1,62 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Exercise 2 
+// Helper functions 
+function isEven(n) {
+  return n % 2 === 0;
+}
 
-// importing data
 
-import { countriesData } from './data/countries'
-import { tenMostHighestPopulations } from './data/ten_most_highest_populations'
+const isPrime = (n) => {
+  if (n <= 3) return n > 1;
+  else if (n % 2 === 0 || n % 3 === 0) return false;
+  else if (n < 25) return true;
+  let i = 5;
+  while (i * i <= n ) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+    i += 6;
+  }
+  return true;
+}
 
-const countries = [
-  { name: 'Finland', city: 'Helsinki' },
-  { name: 'Sweden', city: 'Stockholm' },
-  { name: 'Denmark', city: 'Copenhagen' },
-  { name: 'Norway', city: 'Oslo' },
-  { name: 'Iceland', city: 'Reykjavík' },
-]
-
-// Country component
-const Country = ({ country: { name, city } }) => {
+const Box = ({ color, content }) => {
   return (
-    <div>
-      <h1>{name}</h1>
-      <small>{city}</small>
+    <div style={{ backgroundColor: color }} className='box'>
+      <h4 style={{fontSize: '2em'}}>{ content }</h4>
     </div>
   )
 }
 
-// countries component
-const Countries = ({ countries }) => {
-  const countryList = countries.map((country) => (
-    <Country key={country.name} country={country} />
-  ))
-  return <div>{countryList}</div>
-}
+const NumberBoxes = ( { boxCount }) => {
+  const numberArr = new Array(boxCount).fill(0);
 
+  return (
+    <main>
+      <h3 className='component-title'>Number Generator</h3>;
+
+      <div className='number-boxes'>
+        {numberArr.map((_, index) => {
+            return (
+              isPrime(index) ? 
+                <Box color = "#fd5e53" content={index} key={index}/> : 
+                isEven(index)? 
+                  <Box color = "#21bf73" content={index} key={index}/> : 
+                  <Box color = "#fddb3a" content={index} key={index}/> 
+            )
+          })
+        }
+      </div>
+    </main>
+  )
+}
 // The App, or the parent or the container component
 // Functional Component
 const App = () => {
   return (
     <div className='app'>
-      <div>
-        <h1>Countries List</h1>
-        <Countries countries={countries} />
-      </div>
+      <h1 className='title'>30 Days of React</h1>
+      <NumberBoxes boxCount={32} />
     </div>
   )
 }
